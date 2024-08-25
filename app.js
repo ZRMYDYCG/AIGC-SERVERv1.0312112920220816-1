ab2759f1a42d700f5b66b1f9d1df83cf06fbfce2
ZRMYDYCG <547471919@qq.com>
2024-08-25 21:13:59 +0800

    feat: init

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const bodyParser = require('koa-bodyparser')
 const cors =  require('@koa/cors')
 const Router = require('@koa/router')
 const router = new Router()
-
 app.use(json())
 app.use(bodyParser())
 app.use(cors())
+router.post('/api', ctx => {
+  const { name, age } = ctx.request.body
+  console.log(name, age)
+})
 app.use(router.routes()).use(router.allowedMethods())
-
 app.listen(3030, () => {
   console.log('Server is running on port 3000 🚀')
 })
