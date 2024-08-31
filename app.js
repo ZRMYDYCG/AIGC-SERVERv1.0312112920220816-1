f04c432e4075fec9f00f0503b5a19e3fba90119f
ZRMYDYCG <547471919@qq.com>
2024-08-31 21:23:45 +0800

    feat: 捕获错误中间件

--- app.js
+++ app.js
@@ -24,14 +24,17 @@ const json = require('koa-json')
 const bodyParser = require('koa-bodyparser')
 // 统一接口响应数据格式:中间件
 const responseHandler = require("@/config/result")
+// 捕获错误中间件
+const errorHandler = require("@/config/errorhandler")
 
-// 注册中间件
+// 注册全局中间件
 app.use(json())
 app.use(bodyParser())
 app.use(cors())
+app.use(responseHandler)
+app.use(errorHandler);
 
 // 注册路由
-
 app.use(router.routes()).use(router.allowedMethods())
 
 // 启动服务
---
+++ config/errorhandler.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: ZRMYDYCG
+ * @Date: 2024-08
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2024-08
+ * @Description: 捕获错误的中间件（全局中间件）
+ */
+const errorHandler = async (ctx, next) => {
+    try {
+        await next()
+    } catch (errorData) {
+        const error = errprData.message || "异常错误,请查看服务器端日志"
+        const status = errprData.status || 500
+        ctx.send(null, status, "服务器端异常错误", error)
+    }
+}
